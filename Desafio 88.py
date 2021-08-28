027b295a2b9f3dd9214d6e3312bf6fd392863930
CoelhoEdu <educs98@outlook.com>
2021-08-27 22:36:36 -0300

    Atualizando

--- Desafio 88.py
+++ Desafio 88.py
@@ -1,22 +1,22 @@
 import time
 from random import randint
-print('-#'*50)
+
+print('-#' * 50)
 print('                         JOGA NA MEGA SENA                         ')
-print('-#'*50)
+print('-#' * 50)
 ndejogos = int(input('Quantos jogos você quer eu sorteie ? '))
-print('-#'*50)
+print('-#' * 50)
 for n in range(0, ndejogos):
     jogos = [randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60), randint(1, 60)]
     time.sleep(0.5)
     jogos.sort()
-    print(f'jogo {n+1}: {jogos}')
-print('-#'*50)
+    print(f'jogo {n + 1}: {jogos}')
+print('-#' * 50)
 print('                         JOGOS SORTEADOS, BOA SORTE                        ')
 
-
-#------------------------------------------RESOLUÇÃO DO PROF -----------------------------------------#
+# ------------------------------------------RESOLUÇÃO DO PROF -----------------------------------------#
 lista = list()
-jogos1 = list ()
+jogos1 = list()
 quant = int(input('Quantos jogos voce quer que eu sorteie ? '))
 tot = 1
 while tot <= quant:
@@ -34,6 +34,5 @@ while tot <= quant:
     tot += 1
 print(f'sorteando {quant} jogos')
 for i, l in enumerate(jogos1):
-    print(f'Jogo {i+1}: {l}')
+    print(f'Jogo {i + 1}: {l}')
     time.sleep(1)
-
